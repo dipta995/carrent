c22e6d72153d2157e9a1e4f350a5d82f13e77f7e
dipta995 <dipta995@gmail.com>
2022-01-11 23:57:33 +0600

    working with ordering

--- order.php
+++ order.php
@@ -1,8 +1,7 @@
 <?php include 'header.php';
 
- if (empty($_GET['carid']) || $_GET['carid']==NULL|| !isset($_GET['carid'])) {
+ 			if (empty($_GET['carid']) || $_GET['carid']==NULL|| !isset($_GET['carid'])) {
                     echo "<script>window.location='car.php';</script>";
-         
                 }
                 else {
                     $carid=$_GET['carid'];
@@ -12,296 +11,82 @@
                         $value = mysqli_fetch_array($result);
                     }
                 }
-                  
+if (isset($_POST['confirmorder'])){
+	$user_id =$_SESSION['id'];
+	$car_id =$value['id'];
+	$dropup_location = $_POST['dropup_location'];
+	$pickup_location = $_POST['pickup_location'];
+  
+	$trip_loop = $_POST['trip_loop'];
+	if (empty($car_id)) {
+		echo "Field must not be empty";
+	}else{
+		$sql = "INSERT INTO orders (car_id,user_id,pickup_location,dropup_location,trip_loop,status)VALUES('$car_id','$user_id','$pickup_location','$dropup_location','$trip_loop','$status')";
+		if ($con->query($sql) === TRUE) {
+			echo "<script>window.location='orderlist.php';</script>";
+		 }else {
+			 echo "something wrong";
+		 }
+	}
+}
+
  ?>
 		
-
-		<section class="ftco-section ftco-car-details">
+		<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
       <div class="container">
-      	<div class="row justify-content-center">
-      		<div class="col-md-12">
-      			<div class="car-details">
-      				<div class="img rounded" style="background-image: url(<?php echo $value['image']; ?>);"></div>
-      				<div class="text text-center">
-      					<span class="subheading"><?php echo $value['model']; ?></span>
-      					<h2><?php echo $value['name']; ?></h2>
-      				</div>
-      			</div>
-      		</div>
-      	</div>
-      	<div class="row">
-      		<div class="col-md d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services">
-              <div class="media-body py-md-4">
-              	<div class="d-flex mb-3 align-items-center">
-	              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-dashboard"></span></div>
-	              	<div class="text">
-		                <h3 class="heading mb-0 pl-3">
-		                	Mileage
-		                	<span><?php echo $value['mileage']; ?> /Litter</span>
-		                </h3>
-	                </div>
-                </div>
-              </div>
-            </div>      
+        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+          <div class="col-md-9 ftco-animate pb-5">
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Contact <i class="ion-ios-arrow-forward"></i></span></p>
+            <h1 class="mb-3 bread">Contact Us</h1>
           </div>
-          <div class="col-md d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services">
-              <div class="media-body py-md-4">
-              	<div class="d-flex mb-3 align-items-center">
-	              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-pistons"></span></div>
-	              	<div class="text">
-		                <h3 class="heading mb-0 pl-3">
-		                	Transmission
-		                	<span><?php echo $value['transmission']; ?></span>
-		                </h3>
-	                </div>
-                </div>
-              </div>
-            </div>      
-          </div>
-          <div class="col-md d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services">
-              <div class="media-body py-md-4">
-              	<div class="d-flex mb-3 align-items-center">
-	              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-car-seat"></span></div>
-	              	<div class="text">
-		                <h3 class="heading mb-0 pl-3">
-		                	Seats
-		                	<span><?php echo $value['seats']; ?> Adults</span>
-		                </h3>
-	                </div>
-                </div>
-              </div>
-            </div>      
-          </div>
-          <div class="col-md d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services">
-              <div class="media-body py-md-4">
-              	<div class="d-flex mb-3 align-items-center">
-	              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-backpack"></span></div>
-	              	<div class="text">
-		                <h3 class="heading mb-0 pl-3">
-		                	Luggage
-		                	<span><?php echo $value['luggage']; ?> Bags</span>
-		                </h3>
-	                </div>
-                </div>
-              </div>
-            </div>      
-          </div>
-          <div class="col-md d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services">
-              <div class="media-body py-md-4">
-              	<div class="d-flex mb-3 align-items-center">
-	              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-diesel"></span></div>
-	              	<div class="text">
-		                <h3 class="heading mb-0 pl-3">
-		                	Fuel
-		                	<span><?php echo $value['fuel']; ?></span>
-		                </h3>
-	                </div>
-                </div>
-              </div>
-            </div>      
-          </div>
-      	</div>
-      	<div class="row">
-      		<div class="col-md-12 pills">
-						<div class="bd-example bd-example-tabs">
-							<div class="d-flex justify-content-center">
-							  <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-
-							    <li class="nav-item">
-							      <a class="nav-link active" id="pills-description-tab" data-toggle="pill" href="#pills-description" role="tab" aria-controls="pills-description" aria-expanded="true">Features</a>
-							    </li>
-							    <li class="nav-item">
-							      <a class="nav-link" id="pills-manufacturer-tab" data-toggle="pill" href="#pills-manufacturer" role="tab" aria-controls="pills-manufacturer" aria-expanded="true">Description</a>
-							    </li>
-							    <li class="nav-item">
-							      <a class="nav-link" id="pills-review-tab" data-toggle="pill" href="#pills-review" role="tab" aria-controls="pills-review" aria-expanded="true">Review</a>
-							    </li>
-							  </ul>
-							</div>
-							<style type="text/css">
-								.ion-ios-close{
-									color: red !important;
-								}
-							</style>
-
-						  <div class="tab-content" id="pills-tabContent">
-						    <div class="tab-pane fade show active" id="pills-description" role="tabpanel" aria-labelledby="pills-description-tab">
-
-						    	<div class="row">
-						    		<div class="col-md-4">
-						    			<ul class="features">
-						    				<li class="check"><span class="<?php echo ($value['airconditions']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Airconditions</li>
-						    				<li class="check"><span class="<?php echo ($value['child_seat']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Child Seat</li>
-						    				<li class="check"><span class="<?php echo ($value['gps']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>GPS</li>
-						    				<li class="check"><span class="<?php echo ($value['luggage']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Luggage</li>
-						    				<li class="check"><span class="<?php echo ($value['music']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Music</li>
-						    			</ul>
-						    		</div>
-						    		<div class="col-md-4">
-						    			<ul class="features">
-						    				<li class="check"><span class="<?php echo ($value['seat_belt']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Seat Belt</li>
-						    				<li class="remove"><span class="<?php echo ($value['sleeping_bed']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Sleeping Bed</li>
-						    				<li class="check"><span class="<?php echo ($value['water']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Water</li>
-						    				<li class="check"><span class="<?php echo ($value['bluetooth']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Bluetooth</li>
-						    				<li class="remove"><span class="<?php echo ($value['onboard_computer']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Onboard computer</li>
-						    			</ul>
-						    		</div>
-						    		<div class="col-md-4">
-						    			<ul class="features">
-						    				<li class="check"><span class="<?php echo ($value['audio_input']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Audio input</li>
-						    				<li class="check"><span class="<?php echo ($value['long_term_trips']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Long Term Trips</li>
-						    				<li class="check"><span class="<?php echo ($value['car_kit']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Car Kit</li>
-						    				<li class="check"><span class="<?php echo ($value['remote_central_locking']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Remote central locking</li>
-						    				<li class="check"><span class="<?php echo ($value['climate_control']==1) ? "ion-ios-checkmark" : "ion-ios-close"; ?>"></span>Climate control</li>
-						    			</ul>
-						    		</div>
-						    	</div>
-						    </div>
-
-						    <div class="tab-pane fade" id="pills-manufacturer" role="tabpanel" aria-labelledby="pills-manufacturer-tab">
-						      <p><?php echo $value['description']; ?></p>
-						    </div>
-
-						    <div class="tab-pane fade" id="pills-review" role="tabpanel" aria-labelledby="pills-review-tab">
-						      <div class="row">
-							   		<div class="col-md-7">
-							   			<h3 class="head">23 Reviews</h3>
-							   			<div class="review d-flex">
-									   		<div class="user-img" style="background-image: url(images/person_1.jpg)"></div>
-									   		<div class="desc">
-									   			<h4>
-									   				<span class="text-left">Jacob Webb</span>
-									   				<span class="text-right">14 March 2018</span>
-									   			</h4>
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-								   					</span>
-								   					<span class="text-right"><a href="#" class="reply"><i class="icon-reply"></i></a></span>
-									   			</p>
-									   			<p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrov</p>
-									   		</div>
-									   	</div>
-									   	<div class="review d-flex">
-									   		<div class="user-img" style="background-image: url(images/person_2.jpg)"></div>
-									   		<div class="desc">
-									   			<h4>
-									   				<span class="text-left">Jacob Webb</span>
-									   				<span class="text-right">14 March 2018</span>
-									   			</h4>
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-								   					</span>
-								   					<span class="text-right"><a href="#" class="reply"><i class="icon-reply"></i></a></span>
-									   			</p>
-									   			<p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrov</p>
-									   		</div>
-									   	</div>
-									   	<div class="review d-flex">
-									   		<div class="user-img" style="background-image: url(images/person_3.jpg)"></div>
-									   		<div class="desc">
-									   			<h4>
-									   				<span class="text-left">Jacob Webb</span>
-									   				<span class="text-right">14 March 2018</span>
-									   			</h4>
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-								   					</span>
-								   					<span class="text-right"><a href="#" class="reply"><i class="icon-reply"></i></a></span>
-									   			</p>
-									   			<p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrov</p>
-									   		</div>
-									   	</div>
-							   		</div>
-							   		<div class="col-md-5">
-							   			<div class="rating-wrap">
-								   			<h3 class="head">Give a Review</h3>
-								   			<div class="wrap">
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					(98%)
-								   					</span>
-								   					<span>20 Reviews</span>
-									   			</p>
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					(85%)
-								   					</span>
-								   					<span>10 Reviews</span>
-									   			</p>
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					(70%)
-								   					</span>
-								   					<span>5 Reviews</span>
-									   			</p>
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					(10%)
-								   					</span>
-								   					<span>0 Reviews</span>
-									   			</p>
-									   			<p class="star">
-									   				<span>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					<i class="ion-ios-star"></i>
-									   					(0%)
-								   					</span>
-								   					<span>0 Reviews</span>
-									   			</p>
-									   		</div>
-								   		</div>
-							   		</div>
-							   	</div>
-						    </div>
-						  </div>
-						</div>
-		      </div>
-				</div>
+        </div>
       </div>
     </section>
+
+    <section class="ftco-section contact-section">
+      <div class="container">
+        <div class="row d-flex mb-5 contact-info">
+        	<div class="col-md-4">
+        		<div class="row mb-5">
+					
+		          <img style="height:200px;width:200;" src="<?php echo $value['image']; ?>" alt="">
+		          
+		        </div>
+          </div>
+          <div class="col-md-8 block-9 mb-md-5">
+            <form action="" method="post" class="bg-light p-5 contact-form">
+              <div class="form-group">
+                <input type="text" required name="pickup_location" class="form-control" placeholder="Pickup Location">
+              </div>
+              <div class="form-group">
+                <input type="text" required name="dropup_location" class="form-control" placeholder="Dropup Location">
+              </div>
+              <div class="form-group">
+				  <select class="form-control" required name="trip_loop" id="">
+					  <option value="">Choose if you want to return</option>
+					  <option value="0">No</option>
+					  <option value="1">Yes</option>
+				  </select>
+              </div>
+             <!--  <div class="form-group">
+                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+              </div> -->
+              <div class="form-group">
+                <input type="submit" name="confirmorder" value="Send Message" class="btn btn-primary py-3 px-5">
+              </div>
+            </form>
+          
+          </div>
+        </div>
+        <div class="row justify-content-center">
+        	<div class="col-md-12">
+        		<div id="map" class="bg-white"></div>
+        	</div>
+        </div>
+      </div>
+    </section>
+	
 
     <section class="ftco-section ftco-no-pt">
     	<div class="container">
