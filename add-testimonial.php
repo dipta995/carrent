85b5f040c9fb6f6854d819c10b6dc86a9ddfa726
dipta995 <dipta995@gmail.com>
2022-05-23 00:30:35 +0600

    address update

--- add-testimonial.php
+++ add-testimonial.php
@@ -1,16 +1,25 @@
-<?php include 'header.php'; ?>
+<?php
+ include 'header.php'; 
+ $query = "SELECT * FROM office_info";
+$result = $con->query($query); 
+$info = mysqli_fetch_array($result);
+
+
+                
+ ?>
+    
 
     <section class="ftco-section contact-section">
       <div class="container">
         <div class="row d-flex mb-5 contact-info">
         	<div class="col-md-4">
-        		<div class="row mb-5">
+          <div class="row mb-5">
 		          <div class="col-md-12">
 		          	<div class="border w-100 p-4 rounded mb-2 d-flex">
 			          	<div class="icon mr-3">
 			          		<span class="icon-map-o"></span>
 			          	</div>
-			            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+			            <p><span>Address:</span><?php  echo $info['address']; ?></p>
 			          </div>
 		          </div>
 		          <div class="col-md-12">
@@ -18,7 +27,7 @@
 			          	<div class="icon mr-3">
 			          		<span class="icon-mobile-phone"></span>
 			          	</div>
-			            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+			            <p><span>Phone:</span> <a href="tel://1234567920"><?php  echo $info['phone']; ?></a></p>
 			          </div>
 		          </div>
 		          <div class="col-md-12">
@@ -26,7 +35,7 @@
 			          	<div class="icon mr-3">
 			          		<span class="icon-envelope-o"></span>
 			          	</div>
-			            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+			            <p><span>Email:</span> <a href="mailto:info@yoursite.com"><?php  echo $info['email']; ?></a></p>
 			          </div>
 		          </div>
 		        </div>
