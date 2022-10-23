a5a9a88b4acd0ed85aa5ea32219a96cb6289c722
Dipta Dey <57134360+dipta995@users.noreply.github.com>
2022-10-23 22:33:11 +0600

    Frontend Index Change

--- admin/header.php
+++ admin/header.php
@@ -43,8 +43,7 @@ if (isset($_GET['logout'])) {
         <!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
             <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+             
             </div>
         </form>
         <!-- Navbar-->
@@ -52,8 +51,6 @@ if (isset($_GET['logout'])) {
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
